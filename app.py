aaa836fc47da4f2f579ecdda21e087a2d5d473f4
vladpaulgame <vlad.paul2506@gmail.com>
2026-02-08 18:55:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def get_all_data():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    # Pe server, portul este setat automat, deci folosim varianta simplă:
+    app.run()
